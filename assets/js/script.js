932e902a45be131885efc746e62decb6a390036e
johnrm <notjohnrm@gmail.com>
2024-04-02 21:02:45 +0000

    Added array and logic, with console output

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+let rpsArray = ['rock', 'paper', 'scissors'];
+
+/**
+ * Clear the decks
+ */
+let computerScore = 0;
+let userScore = 0;
+let computerRPS = "";
+let userRPS = "";
+
+/**
+ * Have a go
+ */
+for (i = 0; i < 5; i++) {
+    computerRPS = computerGuess();
+    userRPS = userGuess();
+    pickWinner()
+}
+//displayWInner()
+//updateScore()
+
+/**
+ * Computer has a guess
+ */
+function computerGuess() {
+    let guess = (Math.floor(Math.random() * 3));
+    console.log("computer: " + guess);
+    return (rpsArray[guess]);
+}
+
+/**
+ * User has a guess
+ */
+function userGuess() {
+    let guess = (Math.floor(Math.random() * 3));
+    console.log("user: " + guess);
+    return (rpsArray[guess]);
+}
+
+function pickWinner() {
+    console.log("Computer guessed " + computerRPS + ".");
+    console.log("User guessed " + userRPS + ".");
+    if (
+        ((computerRPS === "rock") && (userRPS === "scissors")) ||
+        ((computerRPS === "scissors") && (userRPS === "paper")) ||
+        ((computerRPS === "paper") && (userRPS === "rock"))) {
+        console.log("Computer wins!");
+        computerScore += 1;
+    } else if (
+        ((userRPS === "rock" && computerRPS === "scissors")) ||
+        ((userRPS === "scissors" && computerRPS === "paper")) ||
+        ((userRPS === "paper" && computerRPS === "rock"))) {
+        console.log("User wins!");
+        userScore += 1;
+    } else {
+        console.log("Its a draw!");
+        //        computerScore += 1;
+        //        userScore += 1;
+    }
+    console.log("Computer " + computerScore + ". User " + userScore);
+    console.log("");
+}
+
+userScore += 1;
